b4430e3a9228415e480a1399383377119ae58f05
GunwooLee <hutswing5613@gmail.com>
2020-06-29 01:33:54 +0900

    p151

--- src/main/java/com/hutswing/test/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/hutswing/test/springboot/domain/posts/PostsRepository.java
@@ -1,10 +1,13 @@
 package com.hutswing.test.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
 
-    @Query("SeLCE p Frome Posts p ORDER BY p.id DESC")
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
     List<Posts> findAllDesc();
 }
 
